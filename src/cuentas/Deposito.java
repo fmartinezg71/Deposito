00a31ab5e16bbe5a23cdce7101a3446d2ce73c56
marti <marti@FFPMCG>
2021-03-09 12:48:36 +0100

    Primera subida del archivo

---
+++ src/cuentas/Deposito.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuentas;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Deposito {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        cantidad();
+    }
+
+    private static float cantidad() {
+        CCuenta cuenta1;
+        double saldoActual;
+        
+        cuenta1 = new CCuenta("Antonio Lopez","1000-2365-85-123456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+saldoActual );
+        
+        try{
+            cuenta1.retirar(2300);
+        }catch (Exception e) {
+            System.out.println("Fallo al retirar");
+        }
+        try{
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception r) {
+            System.out.println("Fallo al ingresar");
+        }
+        return 0;
+    }
+    
+}
